903eb91ccff4d38d2d3b3dc2143e10c2a8df076e
danielbaek2 <danielbaek2015@gmail.com>
2026-03-26 22:17:26 -0400

    pushing changes to access on laptop

--- perception_3d.py
+++ perception_3d.py
@@ -3,6 +3,8 @@ import open3d
 def preprocess_pointcloud(pcd):
     # remove ground / crop region
     point_cloud = open3d.geometry.PointCloud()
+    indices = point_cloud.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
+    filtered_points = point_cloud.select_by_index(indices)
     return filtered_points
 
 
